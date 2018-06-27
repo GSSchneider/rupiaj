7b893c4f5d2b10c76efbab2a5519850f96d9bc1b
GSSchneider <35821280+GSSchneider@users.noreply.github.com>
2018-06-27 14:25:17 -0400

    Create validateParams function to make putListElementsIntoMap function a bit simpler

--- utils/putListElementsIntoMap.js
+++ utils/putListElementsIntoMap.js
@@ -6,13 +6,11 @@ const chalk = require('chalk');
 
 const logError = chalk.bold.red;
 
-// TODO:  SIMPLIFY this fn!  Maybe create a `validateParameters` fn?
-
 module.exports = function putListElementsIntoMap(list, toPop, numToPop) {
   /** Given an immutable list, returns an immutable map with the elements of said list as its keys and the frequency of their occurrences as their respective values -- with an option to pop off the list as you go.
    *
    * @param {list} immutable list
-   * @param {boolean} toPop - A designation as to whether to pop off the array as the list elements are are put into the map
+   * @param {boolean} toPop - A designation as to whether to pop off the array as the list elements are put into the map
    * @param {boolean} numToPop - Number of items to pop -- and put into the map -- if toPop is true; if not provided, the default is to pop off the entire list.
    * @returns {map} immutable map - The elements in passed-in list, the number of which can be designated by numToPop, as a frequency object in the form of an immutable map.
    * @example
@@ -27,16 +25,32 @@ module.exports = function putListElementsIntoMap(list, toPop, numToPop) {
    *  }
    */
 
-  // set default of numToPop only if toPop is true
-  if (toPop && numToPop === undefined) numToPop = list.size;
+  // CONSIDER:  Should this `validateParams` function be placed outside of this `putListElementsIntoMap` function?
+  const validateParams = (toPop, numToPop) => {
+    if (typeof numToPop === 'number' && numToPop < 0) {
+      throw new Error(
+        logError('numToPop has to be greater than or equal to 0')
+      );
+    }
 
-  if (typeof numToPop === 'number' && numToPop < 0)
-    throw new Error(logError('numToPop has to be greater than or equal to 0'));
+    if (toPop && (numToPop < 0 || numToPop === null)) {
+      throw new Error(
+        logError('toPop and numToPop need to be both truthy or both falsy')
+      ); // so you don't try to do silly things like say you're not going to pop off anything, but then specify a number (other than 0) to pop off
+    }
+  };
 
-  if (!!toPop !== !!numToPop)
-    throw new Error(
-      logError('toPop and numToPop need to be either both truthy or both falsy')
-    ); // so don't try to do silly things like say you're not going to pop off anything, but then specify a number (other than 0) to pop off
+  // // POSSIBLE DELETE:
+  // // Is this even necessary, if I just set do `times = numToPop || list.size`?
+  // const setNumToPopDefault = (toPop, numToPop) => {
+  //   // set default of numToPop only if toPop is true
+  //   if (toPop && numToPop === undefined) {
+  //     numToPop = list.size;
+  //   }
+  // };
+
+  validateParams(toPop, numToPop);
+  // setNumToPopDefault(toPop, numToPop);
 
   let tracker = Map();
   let times = numToPop || list.size;
@@ -52,8 +66,8 @@ module.exports = function putListElementsIntoMap(list, toPop, numToPop) {
     else tracker.set(tracker[currentElement], tracker[currentElement] + 1); // equivalent to `tracker[currentElement]++`
   }
 
-  console.log('final list:', list);
-  console.log('final tracker:', tracker.entries);
+  // console.log('final list:', list);
+  // console.log('final tracker:', tracker.entries);
 
   return tracker;
 };
